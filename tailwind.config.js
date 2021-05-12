970dd0295713870925cc98f565a00ec7b1580032
Hans Kirkendoll <hans.kirkendoll@gmail.com>
2021-05-12 01:20:50 -0400

    added dark mode y not

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,29 @@
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        'ws-blue': {
-          100: '#E6F0FF',
-          200: '#BFDAFF',
-          300: '#99C3FF',
-          400: '#4D97FE',
-          500: '#006AFE',
-          600: '#005FE5',
-          700: '#004098',
-          800: '#003072',
-          900: '#00204C',
-        },
-      }
-    },
-  },
-  variants: {},
-  plugins: [],
-  purge: process.env.NODE_ENV === 'production' ? {
-    enabled: true,
-    content: ['src/**/*.njk', 'src/**/*.js'],
-  } : {}
-}
+	theme: {
+		extend: {
+			colors: {
+				"ws-blue": {
+					100: "#E6F0FF",
+					200: "#BFDAFF",
+					300: "#99C3FF",
+					400: "#4D97FE",
+					500: "#006AFE",
+					600: "#005FE5",
+					700: "#004098",
+					800: "#003072",
+					900: "#00204C",
+				},
+			},
+		},
+	},
+	variants: {},
+	darkMode: "media",
+	plugins: [],
+	purge:
+		process.env.NODE_ENV === "production"
+			? {
+					enabled: true,
+					content: ["src/**/*.njk", "src/**/*.js"],
+			  }
+			: {},
+};
