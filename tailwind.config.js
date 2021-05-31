9f97584fb9e4735267be6e2881c4e1eb10313d8d
Hans Kirkendoll <hans.kirkendoll@gmail.com>
2021-05-31 16:45:01 -0400

    added pics, set up grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,10 @@ module.exports = {
 					"url('https://res.cloudinary.com/dsq8eoc9l/ar_3,w_2000,f_auto,q_auto:eco,c_lfill,g_center,e_blur:2000/v1621867913/wedding_v2/0224_enc5bm')",
 				"hero-2":
 					"url('https://res.cloudinary.com/dsq8eoc9l/ar_3,w_2000,f_auto,q_auto:eco,c_lfill,g_center,e_blur:2000/v1621867913/wedding_v2/0224_enc5bm')",
+				"hero-3":
+					"url('https://res.cloudinary.com/dsq8eoc9l/ar_3,w_2000,f_auto,q_auto:eco,c_lfill,g_center,e_blur:2000/v1622166017/wedding_v2/0170_emhc1x')",
+				"hero-4":
+					"url('https://res.cloudinary.com/dsq8eoc9l/ar_3,w_2000,f_auto,q_auto:eco,c_lfill,g_center,e_blur:2000/v1622142588/wedding_v2/0616_rzkhhw')",
 			}),
 		},
 	},
